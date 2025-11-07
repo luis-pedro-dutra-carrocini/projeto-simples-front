97a136408a6331b08f5253f287ad6ac1cd1d7a46
luis-pedro-dutra-carrocini <luispedrodutracarrocini58@gmail.com>
2025-11-07 19:47:56 -0300

    feat(script): Atualizar rota API

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function chamarAPI() {
-  const resposta = await fetch("https://projeto-simples-back.onrender.com/")
+  const resposta = await fetch("https://projeto-simples-back-4hj5.onrender.com/")
   const dados = await resposta.json()
   document.getElementById("resposta").textContent = dados.message
 }
